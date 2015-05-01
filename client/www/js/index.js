48abe3cca1b52e63a2b6cfb95f9252ea40e16caf
Andrew Kovalenko <andrew@jaybirdgroup.com>
2015-05-01 15:15:22 +0300

    syntax fix

--- client/www/js/index.js
+++ client/www/js/index.js
@@ -52,191 +52,189 @@ app.initialize();
 
 function init() {
 
-  console.log('init...');
+    console.log('init...');
 
-  // PeerJS server location
-  //var SERVER_IP = '192.168.0.102'; //home
-  var SERVER_IP = '192.168.1.227'; //office
-  var SERVER_PORT = 9000;
+    // PeerJS server location
+    //var SERVER_IP = '192.168.0.102'; //home
+    var SERVER_IP = '192.168.1.227'; //office
+    var SERVER_PORT = 9000;
 
-  // DOM elements manipulated as user interacts with the app
-  var messageBox = document.querySelector('#messages');
-  var callerIdEntry = document.querySelector('#caller-id');
-  var connectBtn = document.querySelector('#connect');
-  var recipientIdEntry = document.querySelector('#recipient-id');
-  var dialBtn = document.querySelector('#dial');
-  var remoteVideo = document.querySelector('#remote-video');
-  var localVideo = document.querySelector('#local-video');
+    // DOM elements manipulated as user interacts with the app
+    var messageBox = document.querySelector('#messages');
+    var callerIdEntry = document.querySelector('#caller-id');
+    var connectBtn = document.querySelector('#connect');
+    var recipientIdEntry = document.querySelector('#recipient-id');
+    var dialBtn = document.querySelector('#dial');
+    var remoteVideo = document.querySelector('#remote-video');
+    var localVideo = document.querySelector('#local-video');
 
-  // the ID set for this client
-  var callerId = null;
+    // the ID set for this client
+    var callerId = null;
 
-  // PeerJS object, instantiated when this client connects with its
-  // caller ID
-  var peer = null;
+    // PeerJS object, instantiated when this client connects with its
+    // caller ID
+    var peer = null;
 
-  // the local video stream captured with getUserMedia()
-  var localStream = null;
+    // the local video stream captured with getUserMedia()
+    var localStream = null;
 
-  // DOM utilities
-  var makePara = function (text) {
-    var p = document.createElement('p');
-    p.innerText = text;
-    return p;
-  };
-
-  var addMessage = function (para) {
-    if (messageBox.firstChild) {
-      messageBox.insertBefore(para, messageBox.firstChild);
-    }
-    else {
-      messageBox.appendChild(para);
-    }
-  };
-
-  var logError = function (text) {
-    var p = makePara('ERROR: ' + text);
-    p.style.color = 'red';
-    addMessage(p);
-  };
-
-  var logMessage = function (text) {
-    addMessage(makePara(text));
-  };
-
-  // get the local video and audio stream and show preview in the
-  // "LOCAL" video element
-  // successCb: has the signature successCb(stream); receives
-  // the local video stream as an argument
-  var getLocalStream = function (successCb) {
-    if (localStream && successCb) {
-      successCb(localStream);
-    }
-    else {
-      navigator.webkitGetUserMedia(
-        {
-          audio: true,
-          video: true
-        },
-
-        function (stream) {
-          localStream = stream;
-
-          localVideo.src = window.URL.createObjectURL(stream);
-
-          if (successCb) {
-            successCb(stream);
-          }
-        },
-
-        function (err) {
-          logError('failed to access local camera');
-          logError(err.message);
+    // DOM utilities
+    var makePara = function(text) {
+        var p = document.createElement('p');
+        p.innerText = text;
+        return p;
+    };
+    var addMessage = function(para) {
+        if (messageBox.firstChild) {
+            messageBox.insertBefore(para, messageBox.firstChild);
+        } else {
+            messageBox.appendChild(para);
         }
-      );
-    }
-  };
+    };
 
-  // set the "REMOTE" video element source
-  var showRemoteStream = function (stream) {
-    remoteVideo.src = window.URL.createObjectURL(stream);
-  };
+    var logError = function(text) {
+        var p = makePara('ERROR: ' + text);
+        p.style.color = 'red';
+        addMessage(p);
+    };
 
-  // set caller ID and connect to the PeerJS server
-  var connect = function () {
+    var logMessage = function(text) {
+        addMessage(makePara(text));
+    };
 
-    console.log('connect...');
+    // get the local video and audio stream and show preview in the
+    // "LOCAL" video element
+    // successCb: has the signature successCb(stream); receives
+    // the local video stream as an argument
+    var getLocalStream = function(successCb) {
+        if (localStream && successCb) {
+            successCb(localStream);
+        } else {
+            navigator.webkitGetUserMedia({
+                    audio: true,
+                    video: true
+                },
 
-    callerId = callerIdEntry.value;
+                function(stream) {
+                    localStream = stream;
 
-    if (!callerId) {
-      logError('please set caller ID first');
-      return;
-    }
+                    localVideo.src = window.URL.createObjectURL(stream);
 
-    try {
-      // create connection to the ID server
-      console.log('create connection to the ID server');
-      console.log('host: ' + SERVER_IP + ', port: ' + SERVER_PORT);
-      peer = new Peer(callerId, {host: SERVER_IP, port: SERVER_PORT});
+                    if (successCb) {
+                        successCb(stream);
+                    }
+                },
 
-      // hack to get around the fact that if a server connection cannot
-      // be established, the peer and its socket property both still have
-      // open === true; instead, listen to the wrapped WebSocket
-      // and show an error if its readyState becomes CLOSED
-      peer.socket._socket.onclose = function () {
-        logError('no connection to server');
-        peer = null;
-      };
+                function(err) {
+                    logError('failed to access local camera');
+                    logError(err.message);
+                }
+            );
+        }
+    };
 
-      // get local stream ready for incoming calls once the wrapped
-      // WebSocket is open
-      peer.socket._socket.onopen = function () {
-        getLocalStream();
-      };
+    // set the "REMOTE" video element source
+    var showRemoteStream = function(stream) {
+        remoteVideo.src = window.URL.createObjectURL(stream);
+    };
 
-      // handle events representing incoming calls
-      peer.on('call', answer);
-    }
-    catch (e) {
-      peer = null;
-      logError('error while connecting to server');
-    }
-  };
+    // set caller ID and connect to the PeerJS server
+    var connect = function() {
 
-  // make an outgoing call
-  var dial = function () {
-    if (!peer) {
-      logError('please connect first');
-      return;
-    }
+        console.log('connect...');
 
-    if (!localStream) {
-      logError('could not start call as there is no local camera');
-      return
-    }
+        callerId = callerIdEntry.value;
 
-    var recipientId = recipientIdEntry.value;
+        if (!callerId) {
+            logError('please set caller ID first');
+            return;
+        }
 
-    if (!recipientId) {
-      logError('could not start call as no recipient ID is set');
-      return;
-    }
+        try {
+            // create connection to the ID server
+            console.log('create connection to the ID server');
+            console.log('host: ' + SERVER_IP + ', port: ' + SERVER_PORT);
+            peer = new Peer(callerId, {
+                host: SERVER_IP,
+                port: SERVER_PORT
+            });
 
-    getLocalStream(function (stream) {
-      logMessage('outgoing call initiated');
+            // hack to get around the fact that if a server connection cannot
+            // be established, the peer and its socket property both still have
+            // open === true; instead, listen to the wrapped WebSocket
+            // and show an error if its readyState becomes CLOSED
+            peer.socket._socket.onclose = function() {
+                logError('no connection to server');
+                peer = null;
+            };
 
-      var call = peer.call(recipientId, stream);
+            // get local stream ready for incoming calls once the wrapped
+            // WebSocket is open
+            peer.socket._socket.onopen = function() {
+                getLocalStream();
+            };
 
-      call.on('stream', showRemoteStream);
+            // handle events representing incoming calls
+            peer.on('call', answer);
+        } catch (e) {
+            peer = null;
+            logError('error while connecting to server');
+        }
+    };
 
-      call.on('error', function (e) {
-        logError('error with call');
-        logError(e.message);
-      });
-    });
-  };
+    // make an outgoing call
+    var dial = function() {
+        if (!peer) {
+            logError('please connect first');
+            return;
+        }
 
-  // answer an incoming call
-  var answer = function (call) {
-    if (!peer) {
-      logError('cannot answer a call without a connection');
-      return;
-    }
+        if (!localStream) {
+            logError('could not start call as there is no local camera');
+            return
+        }
 
-    if (!localStream) {
-      logError('could not answer call as there is no localStream ready');
-      return;
-    }
+        var recipientId = recipientIdEntry.value;
 
-    logMessage('incoming call answered');
+        if (!recipientId) {
+            logError('could not start call as no recipient ID is set');
+            return;
+        }
 
-    call.on('stream', showRemoteStream);
+        getLocalStream(function(stream) {
+            logMessage('outgoing call initiated');
 
-    call.answer(localStream);
-  };
+            var call = peer.call(recipientId, stream);
 
-  // wire up button events
-  connectBtn.addEventListener('click', connect);
-  dialBtn.addEventListener('click', dial);
+            call.on('stream', showRemoteStream);
+
+            call.on('error', function(e) {
+                logError('error with call');
+                logError(e.message);
+            });
+        });
+    };
+
+    // answer an incoming call
+    var answer = function(call) {
+        if (!peer) {
+            logError('cannot answer a call without a connection');
+            return;
+        }
+
+        if (!localStream) {
+            logError('could not answer call as there is no localStream ready');
+            return;
+        }
+
+        logMessage('incoming call answered');
+
+        call.on('stream', showRemoteStream);
+
+        call.answer(localStream);
+    };
+
+    // wire up button events
+    connectBtn.addEventListener('click', connect);
+    dialBtn.addEventListener('click', dial);
 }
--- server/index.js
+++ server/index.js
@@ -2,14 +2,17 @@ var ip = require('ip');
 var port = 9000;
 
 var PeerServer = require('peer').PeerServer;
-var server = new PeerServer({port: port, allow_discovery: true});
-
-server.on('connection', function (id) {
-  console.log('new connection with id ' + id);
+var server = new PeerServer({
+    port: port,
+    allow_discovery: true
 });
 
-server.on('disconnect', function (id) {
-  console.log('disconnect with id ' + id);
+server.on('connection', function(id) {
+    console.log('new connection with id ' + id);
+});
+
+server.on('disconnect', function(id) {
+    console.log('disconnect with id ' + id);
 });
 
 console.log('peer server running on ' + ip.address() + ':' + port);
